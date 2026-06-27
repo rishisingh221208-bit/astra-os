1df78ac77b5a495d2664c3528ba95adcab4a9002
rishisingh221208-bit <rishisingh221208@gmail.com>
2026-06-27 21:31:24 +0530

    Update main.rs

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,15 +1,22 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod uart; // Load our new driver
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    uart::print_str("KERNEL PANIC!\n");
     loop {}
 }
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // This is the very first thing Astra-OS will do!
+    uart::print_str("\n===============================\n");
+    uart::print_str(" Astra-OS successfully booted! \n");
+    uart::print_str("===============================\n\n");
+    
+    // Then sit in the infinite loop to stay alive
     loop {}
 }
 
