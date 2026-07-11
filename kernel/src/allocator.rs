ade0fd9185dd82b69820f8abf5775916f7d3f179
rishisingh221208-bit <rishisingh221208@gmail.com>
2026-07-11 19:46:57 +0530

    Modify alloc function to silence unused variable warning
    Silence unused variable warning by prefixing 'layout' with an underscore in the alloc function.

--- kernel/src/allocator.rs
+++ kernel/src/allocator.rs
@@ -7,15 +7,16 @@ use core::ptr::null_mut;
 pub struct AstraAllocator;
 
 unsafe impl GlobalAlloc for AstraAllocator {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+    // Prefix 'layout' with an underscore to silence the unused variable warning
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
         // A simple bump allocator mapping into a safe heap region
         // Starting at a safe space right above our kernel stack
         let heap_start = 0x40800000 as *mut u8;
         
         // For now, return a raw pointer to the heap space
-        // We will expand this with a buddy-system algorithm next!
         heap_start
     }
+    
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
         // Freeing memory back to the system pool
